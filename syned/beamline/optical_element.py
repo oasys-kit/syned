745492e4f98f9c3481b96a36775b82d289f9afd3
luca.rebuffi@elettra.eu <Bacilotto_00>
2017-01-26 13:59:00 +0100

    First commit

--- syned/beamline/optical_element.py
+++ syned/beamline/optical_element.py
@@ -1,6 +1,10 @@
 class OpticalElement(object):
-    def __init__(self, name):
+    def __init__(self, name, boundary_shape=None):
         self._name = name
+        self._boundary_shape = boundary_shape
 
     def get_name(self):
         return self._name
+
+    def get_boundary_shape(self):
+        return self._boundary_shape
--- syned/beamline/optical_elements/mirrors/mirror.py
+++ syned/beamline/optical_elements/mirrors/mirror.py
@@ -1,5 +1,8 @@
 from syned.beamline.optical_elements.shape import SurfaceShape, OpticalElementsWithSurfaceShape
 
 class Mirror(OpticalElementsWithSurfaceShape):
-    def __init__(self, name, surface_shape=SurfaceShape()):
-        super().__init__(name, surface_shape)
+    def __init__(self,
+                 name,
+                 boundary_shape=None,
+                 surface_shape=SurfaceShape()):
+        super().__init__(name, boundary_shape, surface_shape)
