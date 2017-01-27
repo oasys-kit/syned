859c56044e0fd7d86d5cd8ccb43402b498562cba
luca.rebuffi@elettra.eu <Bacilotto_00>
2017-01-27 17:20:12 +0100

    Migration of Waveoptics library from SR-Xraylib

--- syned/beamline/element_coordinates.py
+++ syned/beamline/element_coordinates.py
@@ -3,31 +3,27 @@ Position of a beamline component within a beamline.
 """
 
 class ElementCoordinates(object):
-    def __init__(self, z = 0.0, x=0.0, y=0.0, angle_radial=0.0, angle_azimuthal=0.0):
+    def __init__(self, p = 0.0, q = 0.0, angle_radial=0.0, angle_azimuthal=0.0):
         """
 
-        :param z: Longitudinal position.
-        :param x: Off-axis shift in vertical direction.
-        :param y: Off-axis shift in horizontal direction.
+        :param p: distance from previous element.
+        :param q: distance to next element.
         :param angle_radial: Radial inclination angle.
         :param angle_azimuthal: Azimuthal inclination angle.
         :return:
         """
-        self._z = z
-        self._x = x
-        self._y = y
+        self._p = p
+        self._q = q
 
         self._angle_radial = angle_radial
         self._angle_azimuthal = angle_azimuthal
 
-    def x(self):
-        return self._x
 
-    def y(self):
-        return self._y
+    def p(self):
+        return self._p
 
-    def z(self):
-        return self._z
+    def q(self):
+        return self._q
 
     def angleRadial(self):
         return self._angle_radial
