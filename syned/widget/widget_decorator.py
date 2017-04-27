550aa96044cad638dc40bcd91a8d011565b1af32
luca.rebuffi@elettra.eu <Bacilotto_00>
2017-04-27 13:06:49 +0200

    Syned Widgets

--- syned/widget/widget_decorator.py
+++ syned/widget/widget_decorator.py
@@ -1,10 +1,11 @@
 
+from syned.beamline.beamline import Beamline
 
 class WidgetDecorator(object):
 
     @classmethod
     def syned_input_data(cls):
-        return ("Syned Data", object, "receive_syned_data")
+        return ("Syned Beamline", Beamline, "receive_syned_data")
 
     def receive_syned_data(self, data):
         raise NotImplementedError("Should be implemented")
