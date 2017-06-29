3ced8ded78f01dd9ece835356d3eb0a65f4d70f1
Manuel Sanchez del Rio <srio@esrf.eu>
2017-06-29 14:22:51 +0200

    debug

--- syned/beamline/optical_elements/ideal_elements/lens.py
+++ syned/beamline/optical_elements/ideal_elements/lens.py
@@ -7,14 +7,14 @@ from syned.beamline.optical_element import OpticalElement
 
 
 class IdealLens(OpticalElement):
-    def __init__(self, name="Undefined", focal_y=1.0, focal_x=None, ):
+    def __init__(self, name="Undefined", focal_x=None, focal_y=1.0, ):
         OpticalElement.__init__(self, name=name)
-        self._focal_y = focal_y
         self._focal_x = focal_x
+        self._focal_y = focal_y
         # support text containg name of variable, help text and unit. Will be stored in self._support_dictionary
         self._set_support_text([
-                    ("focal_y"      , "Focal length in y [vertical]",    "m" ),
                     ("focal_x"      , "Focal length in x [horizontal]", "m" ),
+                    ("focal_y"      , "Focal length in y [vertical]",    "m" ),
             ] )
 
     def focal_x(self):
