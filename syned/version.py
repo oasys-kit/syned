7e2a8f3dfb4a11eecaa136b9a43eb554691c6af5
lucarebuffi <lrebuffi@anl.gov>
2019-10-03 11:34:04 -0500

    New Features

--- syned/version.py
+++ syned/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM syned SETUP.PY
 short_version = '1.0.13'
 version = '1.0.13'
-full_version = '1.0.13.dev0+66499a1'
-git_revision = '66499a16e4ff431a618e68b51a2da6645e97c520'
+full_version = '1.0.13.dev0+3018e55'
+git_revision = '3018e55a5571ae5ec57aba1feaa7af44986d8bfb'
 release = False
 
 if not release:
