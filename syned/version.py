d893fe8a2416f89237f8e70ebe46cb0ef21530c9
Manuel Sanchez del Rio <srio@esrf.eu>
2019-10-10 10:42:35 -0700

    upgraded version

--- syned/version.py
+++ syned/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM syned SETUP.PY
-short_version = '1.0.13'
-version = '1.0.13'
-full_version = '1.0.13.dev0+3018e55'
-git_revision = '3018e55a5571ae5ec57aba1feaa7af44986d8bfb'
+short_version = '1.0.14'
+version = '1.0.14'
+full_version = '1.0.14.dev0+7e2a8f3'
+git_revision = '7e2a8f3dfb4a11eecaa136b9a43eb554691c6af5'
 release = False
 
 if not release:
